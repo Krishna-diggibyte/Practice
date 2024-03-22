12a98768fe5c85d7ec5c07e15708095bfb278dd5
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-03-22 16:50:56 +0530

    use map and mapPartitionWithIndex

---
+++ pyspark/rdd/map_in_rdd.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+
+sc = SparkSession.builder.master("local[1]").appName("Krishna").getOrCreate()
+
+# Create two RDD
+rdd1 = sc.sparkContext.parallelize([1, 2, 3, 4, 5])
+rdd2 = sc.sparkContext.parallelize([1, 2, 3, 4, 5], 2)  # 2 partitions [1,2][3,4,5]
+
+# Apply map operation to double each element
+doubled_rdd = rdd1.map(lambda x: x * 2)
+
+# Apply mapPartitionsWithIndex operation to double each element in each partition
+mapped_rdd = rdd2.mapPartitionsWithIndex(lambda index, partition: ((index, x * 2) for x in partition))
+
+# Print the result
+print(doubled_rdd.collect())
+
+print(mapped_rdd.collect())
