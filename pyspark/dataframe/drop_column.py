87a58e14cc6c36e31baa7a26e3f1eaa2f681c2ef
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-03-26 13:45:31 +0530

    drop column from data frame

---
+++ pyspark/dataframe/drop_column.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import lit
+
+# Initialize SparkSession
+spark = SparkSession.builder.appName("Krishna").getOrCreate()
+
+# Create a DataFrame
+data = [("Krishna",22,25000),("Kuldeep",23,24999),("Ravi",24,26000),("Amit",21,25000)]
+columns = ["Name", "Age","CTC"]
+df = spark.createDataFrame(data, columns)
+
+df.show()
+new_df=df.drop("CTC")
+new_df.show()
