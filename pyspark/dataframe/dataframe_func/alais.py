fcbb8751909cd0aa3a62299fb8bcf1c79c49900a
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-03-28 10:23:28 +0530

    alais function

---
+++ pyspark/dataframe/dataframe_func/alais.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as f
+
+spark=SparkSession.builder.master("local[1]").appName("Krishna").getOrCreate()
+
+emp_data = [("Krishna","Sales","UK",90000,23,10000),
+    ("Kuldeep","Sales","KA",86000,22,20000),
+    ("Basheer","Sales","TN",81000,24,23000),
+    ("Pratibha","Finance","MP",90000,21,23000),
+    ("Kalai","Finance","TN",99000,24,24000),
+    ("Manvi","Finance","RJ",83000,21,19000),
+    ("Ravi","Finance","UK",79000,25,15000),
+    ("Amit","Marketing","WB",80000,22,18000),
+    ("Pankaj","Marketing","DL",91000,26,21000)
+  ]
+schema = ["employee_name","department","state","salary","age","bonus"]
+
+
+df = spark.createDataFrame(data=emp_data, schema = schema)
+
+# df.show(truncate=False)
+
+df.groupBy("department").agg(f.sum("salary").alias("Total sum")).show()
+
