c0c1772225f5599589242ea957ad0428b8d6bd74
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-03-28 19:11:12 +0530

    read csv file with schema

--- pyspark/dataframe/reading_different_file_formats/read_csv.py
+++ pyspark/dataframe/reading_different_file_formats/read_csv.py
@@ -1,17 +1,34 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 
 sc=SparkSession.builder.master("local").appName("Krishna").getOrCreate()
 
-df_without_header=sc.read.csv("emp_data.csv")
+
+schema=StructType([
+    StructField("emp_id",StringType(),True),
+    StructField("name",StringType(),True),
+    StructField("age",IntegerType() ,True)
+])
+# with schema
+df_with_schema=sc.read.csv("emp_data2.csv",schema=schema)
+
+#with inferschema
 df_with_header=sc.read.csv("emp_data.csv",header=True,inferSchema=True)
+
+#without schema
 df_with_option = sc.read.option("header", True).csv("emp_data.csv")
 
+#withschema
+df_with_option_schema=sc.read.schema(schema).option("header",False).csv("emp_data2.csv")
 
-df_without_header.printSchema()
-df_without_header.show(truncate=False)
+
+df_with_schema.printSchema()
+df_with_schema.show(truncate=False)
 df_with_header.printSchema()
 df_with_header.show(truncate=False)
 
-
 df_with_option.printSchema()
 df_with_option.show()
+
+df_with_option_schema.printSchema()
+df_with_option_schema.show()
