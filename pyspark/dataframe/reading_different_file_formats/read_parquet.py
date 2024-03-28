34140ff1c7b4dc1e53b160c89015bc982a80f752
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-03-28 19:11:31 +0530

    read parquet file with schema

--- pyspark/dataframe/reading_different_file_formats/read_parquet.py
+++ pyspark/dataframe/reading_different_file_formats/read_parquet.py
@@ -1,16 +1,23 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
 spark=SparkSession.builder.master("local[1]").appName("Krishna").getOrCreate()
+
+schema=StructType([
+    StructField("Emp_id",StringType(),True),
+    StructField("Name",StringType(),True),
+    StructField("Age",IntegerType() ,True)
+])
 
 df_read=spark.read.parquet("emp_data.parquet")
 
 df_read.printSchema()
 df_read.show()
 
-df_with_option = spark.read.option("header", True).parquet("emp_data.parquet")
-
-df_with_option.printSchema()
-df_with_option.show()
+# df_with_option = spark.read.option("header", False).parquet("emp_data.parquet")
+#
+# df_with_option.printSchema()
+# df_with_option.show()
 
 
 
