b188f97ef65df42b85e9e710b0f264a14887745a
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-04-01 13:01:10 +0530

    read parquest file with custom schema

--- pyspark/dataframe/reading_different_file_formats/read_parquet.py
+++ pyspark/dataframe/reading_different_file_formats/read_parquet.py
@@ -3,25 +3,21 @@ from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
 spark=SparkSession.builder.master("local[1]").appName("Krishna").getOrCreate()
 
-schema=StructType([
-    StructField("Emp_id",StringType(),True),
-    StructField("Name",StringType(),True),
-    StructField("Age",IntegerType() ,True)
-])
+# schema=StructType([
+#     StructField("Emp_id",StringType(),True),
+#     StructField("Name",StringType(),True),
+#     StructField("Age",IntegerType() ,True)
+# ])
 
-df_read=spark.read.parquet("emp_data.parquet")
+df_read=spark.read.parquet("../../resource/emp_data.parquet")
 
 df_read.printSchema()
 df_read.show()
 
-# df_with_option = spark.read.option("header", False).parquet("emp_data.parquet")
-#
-# df_with_option.printSchema()
-# df_with_option.show()
+df_with_option = spark.read.option("header", False).parquet("../../resource/emp_data.parquet")
 
-
-
-# df_read.write.format("csv").mode("overwrite").save("sample/emp_data1.csv")
+df_with_option.printSchema()
+df_with_option.show()
 
 
 
