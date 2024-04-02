cd4cab830a17b9a1492870b16579276d2064a561
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-04-02 21:03:20 +0530

    array intersect function

---
+++ pyspark/dataframe/array_function/array_intersect.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import array_intersect
+from pyspark.sql.types import StringType,StructType,StructField,IntegerType, ArrayType
+
+# Initialize SparkSession
+spark = SparkSession.builder.master("local[*]").appName("Krishna").getOrCreate()
+
+# Sample DataFrame
+data = [("Krishna", [1, 2, 3, 4,7],[7,3,8,9]),
+        ("Kuldeep", [5, 6, 7, 8],[6,2,3]),
+        ("Basheer", [1, 2, 3, 4, 5],[0,7,2,5])]
+schema=StructType([
+    StructField("name",StringType(),True),
+    StructField("num_1",ArrayType(IntegerType()),True),
+    StructField("num_2",ArrayType(IntegerType()),True)
+])
+
+new_df = spark.createDataFrame(data=data,schema=schema)
+
+new_df.show()
+
+# array intersect
+
+new_df.withColumn("Intersect",array_intersect("num_1","num_2")).show()
