0ac4949aecbf40a5e66281f8d4f10beb339f4698
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-04-02 20:28:17 +0530

    union in dataframe

---
+++ pyspark/dataframe/union_intersection/union.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+sc= SparkSession.builder.master("local").appName("Krishna").getOrCreate()
+
+data1 = [(1,"Krishna","40","M"), \
+    (2,"Basheer","20","M"), \
+    (3,"Kuldeep","10","M"), \
+    (4,"Pratibhia","10","F") \
+  ]
+column = ["emp_id","name","emp_dept_id","gender"]
+
+data2 = [(1,"Krishna","40","M"), \
+    (5,"Manvi","40",""), \
+      (6,"Amit","50","") \
+  ]
+
+empDF1 = sc.createDataFrame(data=data1, schema = column)
+empDF2 = sc.createDataFrame(data=data2, schema = column)
+
+
+
+# union_intersection
+
+uniondf=empDF1.union(empDF2)
+uniondf.show()
