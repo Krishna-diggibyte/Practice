e3cc8f2f69a781539869fe12be05e218cdba50cf
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-04-02 20:22:50 +0530

    array function to add new column

---
+++ pyspark/dataframe/array_function/array.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import array, array_contains, array_size, array_position, array_remove, lit, col
+from pyspark.sql.types import StringType,StructType,StructField,IntegerType, ArrayType
+
+# Initialize SparkSession
+spark = SparkSession.builder.master("local[*]").appName("Krishna").getOrCreate()
+
+# Sample DataFrame
+data = [("Krishna", [1, 2, 3, 4,7]),
+        ("Kuldeep", [5, 6, 7, 8]),
+        ("Basheer", [1, 2, 3, 4, 5])]
+schema=StructType([
+    StructField("name",StringType(),True),
+    StructField("numbers",ArrayType(IntegerType()),True)
+])
+
+new_df = spark.createDataFrame(data=data,schema=schema)
+
+
+# Using ARRAY function to create a new column as array
+new_df.withColumn("NewArray", array(col('name'))).show()
