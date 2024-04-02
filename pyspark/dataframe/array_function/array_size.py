4021433bddc989a7019b50791fcac929b917d79c
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-04-02 20:08:37 +0530

    array size to find length of array

---
+++ pyspark/dataframe/array_function/array_size.py
@@ -0,0 +1,23 @@
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import array, array_contains, array_size, array_position, array_remove, lit, col
+from pyspark.sql.types import StringType,StructType,StructField,IntegerType, ArrayType
+
+# Initialize SparkSession
+spark = SparkSession.builder.master("local[*]").appName("Krishna").getOrCreate()
+
+# Sample DataFrame
+data = [("Krishna", [1, 2, 3, 4,7]),
+        ("Kuldeep", [5, 6, 7, 8]),
+        ("Basheer", [1, 2, 3, 4, 5])]
+schema=StructType([
+    StructField("name",StringType(),True),
+    StructField("numbers",ArrayType(IntegerType()),True)
+])
+
+new_df = spark.createDataFrame(data=data,schema=schema)
+
+
+# Using ARRAY_LENGTH function to get the length of an array
+new_df.withColumn("Length", array_size("Numbers")).show()
+
