626545ffb84517d8560d4dc8890c8978401ba2ea
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-04-03 21:18:26 +0530

    updated mapp in rdd

--- pyspark/rdd/map_in_rdd.py
+++ pyspark/rdd/map_in_rdd.py
@@ -9,6 +9,11 @@ rdd2 = sc.sparkContext.parallelize([1, 2, 3, 4, 5], 2)  # 2 partitions [1,2][3,4
 # Apply map operation to double each element
 doubled_rdd = rdd1.map(lambda x: x * 2)
 
+# Apply mappartition operation to double each element
+def f(iterator): yield sum(iterator)
+map_rdd = rdd2.mapPartitions(f).glom().collect()
+print(map_rdd)
+
 # Apply mapPartitionsWithIndex operation to double each element in each partition
 mapped_rdd = rdd2.mapPartitionsWithIndex(lambda index, partition: ((index, x * 2) for x in partition))
 
